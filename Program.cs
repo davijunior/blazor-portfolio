fde6adbe1696d79a234d357c7f188e6ceba0652e
davi martimiano junior <martimiano.davi@gmail.com>
2021-03-07 18:03:04 -0300

    erro de digitação, retirada do 'i' do Portifolio

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace Portifolio
+namespace Portfolio
 {
     public class Program
     {
